679c85b013f7aa9484987307fe2892bfcf0750f0
Jiwoo <jsdev887@gmail.com>
2021-08-23 13:49:49 +0900

    Initial Commit

---
+++ NetworkSwiftUI/NetworkSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkSwiftUIApp.swift
+//  NetworkSwiftUI
+//
+//  Created by jwshin on 2021/08/23.
+//
+
+import SwiftUI
+
+@main
+struct NetworkSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
